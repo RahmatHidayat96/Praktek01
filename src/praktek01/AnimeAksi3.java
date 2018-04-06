bb89fa4292b1f1798e7cb64199cef6bdddb144d3
RahmatHidayat <r.hidayat.fti2016@gmail.com>
2018-04-06 10:08:17 +0800

    Mengisi nilai atribute

--- src/praktek01/AnimeAksi3.java
+++ src/praktek01/AnimeAksi3.java
@@ -12,5 +12,11 @@ package praktek01;
 public class AnimeAksi3 {
     public static void main(String[] args) {
        Anime Mahouka = new Anime();
+       
+       Mahouka.Nama="Mahouka Koukou no Rettousei";
+       Mahouka.Genres="Magic";
+       Mahouka.Sumber="Light Novel";
+       Mahouka.Studios="Madhouse";
+       Mahouka.Producers="Square Enix";
     }
 }
